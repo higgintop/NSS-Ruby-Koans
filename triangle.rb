bf048be6c30f6be72f72068e9e0c197a7e73301f
Allison Higginbotham <higgintop@gmail.com>
2015-04-09 09:13:04 -0500

    Finished Koans

--- triangle.rb
+++ triangle.rb
@@ -14,8 +14,12 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
-end
+  raise TriangleError if a<=0 or b<=0 or c<=0
+  raise TriangleError if a+b<=c or b+c<=a or a+c<=b
+  return :equilateral if a==b and a==c                 
+  return :isosceles if a==b or b==c or a==c
+  :scalene                                                                                                       
+end  
 
 # Error class used in part 2.  No need to change this code.
 class TriangleError < StandardError
